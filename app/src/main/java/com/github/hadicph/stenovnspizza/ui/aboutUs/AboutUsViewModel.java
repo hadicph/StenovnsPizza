4a67622f42a8d2a29dcea30618b902e45ba3560c
Dorinus <Chelmenciuc01>
2021-11-29 20:02:36 +0100

    Innitial commit of About us page. No map yet

--- app/src/main/java/com/github/hadicph/stenovnspizza/ui/aboutUs/AboutUsViewModel.java
+++ app/src/main/java/com/github/hadicph/stenovnspizza/ui/aboutUs/AboutUsViewModel.java
@@ -1,16 +1,32 @@
 package com.github.hadicph.stenovnspizza.ui.aboutUs;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.github.hadicph.stenovnspizza.R;
+
 public class AboutUsViewModel extends ViewModel {
+
+    // Dorin Chira
 
     private MutableLiveData<String> mText;
 
     public AboutUsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+//        mText = new MutableLiveData<>();
+//        mText.setValue("This is about us fragment");
+
+
+
+
+
+
     }
 
     public LiveData<String> getText() {
