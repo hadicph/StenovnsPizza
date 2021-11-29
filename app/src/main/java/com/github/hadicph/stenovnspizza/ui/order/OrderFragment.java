99e2ee6b476083cbec480d86bf9f25638e991799
hadicph <30347830+hadicph@users.noreply.github.com>
2021-11-29 10:54:40 +0100

    Navigation Menu Setup

---
+++ app/src/main/java/com/github/hadicph/stenovnspizza/ui/order/OrderFragment.java
@@ -0,0 +1,45 @@
+package com.github.hadicph.stenovnspizza.ui.order;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.github.hadicph.stenovnspizza.databinding.FragmentSlideshowBinding;
+
+public class OrderFragment extends Fragment {
+
+    private OrderViewModel orderViewModel;
+    private FragmentSlideshowBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        orderViewModel =
+                new ViewModelProvider(this).get(OrderViewModel.class);
+
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textSlideshow;
+        orderViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
