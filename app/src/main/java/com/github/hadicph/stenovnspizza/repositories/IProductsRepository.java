1f3d18f1eb3be1e48f0c1dafab3f7cb2e147aafb
Dorinus <Chelmenciuc01>
2021-12-02 10:29:29 +0100

    Refactored ProductsRepository for a better Solid system Added methods to fetch products by category, for all categories. Some can return null, as database is not fully populated at the moment

--- app/src/main/java/com/github/hadicph/stenovnspizza/repositories/IProductsRepository.java
+++ app/src/main/java/com/github/hadicph/stenovnspizza/repositories/IProductsRepository.java
@@ -5,6 +5,18 @@ import com.github.hadicph.stenovnspizza.models.Product;
 import java.util.List;
 
 public interface IProductsRepository {
-    public List<Product> getDrinks();
     public List<Product> getPizza();
+    public List<Product> getSpecialPizza();
+    public List<Product> getMexicanPizza();
+    public List<Product> getBakedPizza();
+    public List<Product> getChildrensPizza();
+    public List<Product> getRiceDishes();
+    public List<Product> getPizzaSandwich();
+    public List<Product> getBurger();
+    public List<Product> getBarbecueDishes();
+    public List<Product> getHomemadeDurum();
+    public List<Product> getSnaks();
+    public List<Product> getSalad();
+    public List<Product> getDrinks();
+    public List<Product> getDips();
 }
