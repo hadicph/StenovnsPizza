8a2a5ffacb655d1fc89966d81feabf944f097fbe
hadicph <30347830+hadicph@users.noreply.github.com>
2021-12-03 14:27:57 +0100

    Home-Fragment Navigation

--- app/src/main/java/com/github/hadicph/stenovnspizza/MainActivity.java
+++ app/src/main/java/com/github/hadicph/stenovnspizza/MainActivity.java
@@ -3,19 +3,14 @@ package com.github.hadicph.stenovnspizza;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
-
-import com.github.hadicph.stenovnspizza.repositories.IProductsRepository;
-import com.github.hadicph.stenovnspizza.repositories.ProductsRepository;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
-
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.github.hadicph.stenovnspizza.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/github/hadicph/stenovnspizza/ui/home/HomeFragment.java
+++ app/src/main/java/com/github/hadicph/stenovnspizza/ui/home/HomeFragment.java
@@ -4,25 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import com.github.hadicph.stenovnspizza.R;
 import com.github.hadicph.stenovnspizza.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    private Button order,aboutUs;
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        /*final TextView textView = binding.textHome;
+        NavController navController = NavHostFragment.findNavController(this);
+        order = root.findViewById(R.id.order_button);
+        aboutUs = root.findViewById(R.id.aboutUs_button);
+
+        order.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_nav_home_to_nav_order);
+            }
+        });
+        aboutUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_nav_home_to_aboutUs);
+            }
+        });
+        /*
+        final TextView textView = binding.textHome;
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
